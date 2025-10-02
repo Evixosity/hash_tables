fa6b48f65d703a4d8a197326e50dfc055c19f0f1
Evixosity <bilguun.soyombo@gmail.com>
2025-10-02 12:48:01 +0800

    Generated hash tables

---
+++ bin/hashtables.dart
@@ -0,0 +1,5 @@
+import '../lib/hashtables.dart' as hashtables;
+
+void main(List<String> arguments) {
+  print('Hello world: ${hashtables.calculate()}!');
+}
---
+++ test/hashtables_test.dart
@@ -0,0 +1,8 @@
+import '../lib/hashtables.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
